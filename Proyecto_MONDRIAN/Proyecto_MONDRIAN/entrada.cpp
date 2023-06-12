ca0b8c9330397187e1c063106eb44a4aa831aac0
Esiil <esiilradiru@gmail.com>
2023-06-12 15:30:35 +0200

    Arreglados errores de git

--- Proyecto_MONDRIAN/Proyecto_MONDRIAN/entrada.cpp
+++ Proyecto_MONDRIAN/Proyecto_MONDRIAN/entrada.cpp
@@ -3,19 +3,57 @@
 using namespace std; 
 int main(int argc, char **argv){ 
  
+	// Se inicia el entorno gráfico
 	iniciarM();
-	 nuevoCuadroM("Homenaje a Mondrian"); 
- 	 rectanguloM(0, 0, 32, 100, 6); 
-	 rectanguloM(87, 8, 32, 100, 6); 
-	 lineaM(80, 4, false, 1); 
-	 pausaM(2.30); 
-	 pausaM(1.89); 
-	 // "Se pintan los recuadros de""Se pintan los recuadros de"tiempoPausa 
-	 // "Hola que tal" 
-	 nuevoCuadroM("Bandera"); 
- 	 rectanguloM(0, 0, 32, 100, 6); 
-	 rectanguloM(68, 0, 32, 100, 1); 
-	 lineaM(90, 4, false, 1); 
+	 nuevoCuadroM("Composición en rojo, amarillo, azul y negro - Mondrian"); 
+ 	 // "Se pintan las lineas horizontales y verticales" 
+	 lineaM(11, 4, true, 1); 
+	 lineaM(34, 4, true, 1); 
+	 lineaM(58, 4, true, 1); 
+	 lineaM(71, 4, true, 1); 
+	 lineaM(84, 4, true, 1); 
+	 lineaM(11, 4, false, 1); 
+	 lineaM(24, 4, false, 1); 
+	 lineaM(38, 4, false, 1); 
+	 lineaM(58, 4, false, 1); 
+	 lineaM(71, 4, false, 1); 
+	 lineaM(86, 4, false, 1); 
+	 pausaM(1.77); 
+	 // "Se pintan los recuadros de color" 
+	 rectanguloM(15, 15, 43, 43, 3); 
+	 rectanguloM(62, 15, 22, 23, 1); 
+	 rectanguloM(15, 62, 19, 24, 5); 
+	 rectanguloM(0, 62, 11, 24, 5); 
+	 rectanguloM(75, 0, 25, 11, 5); 
+	 rectanguloM(88, 42, 12, 16, 1); 
+	 rectanguloM(75, 62, 25, 24, 4); 
+	 rectanguloM(75, 90, 25, 11, 3); 
+	 pausaM(4.43); 
+	 // "Se pintan recuadros blancos para borrar parte de la rejilla" 
+	 rectanguloM(0, 11, 11, 4, 0); 
+	 rectanguloM(0, 38, 11, 4, 0); 
+	 rectanguloM(58, 0, 4, 11, 0); 
+	 rectanguloM(88, 24, 12, 4, 0); 
+	 rectanguloM(62, 71, 9, 4, 0); 
+	 rectanguloM(0, 90, 71, 10, 0); 
+	 pausaM(8.85); 
+	 nuevoCuadroM("Homenaje a Rothko"); 
+ 	 rectanguloM(0, 0, 100, 100, 2); 
+	 nuevoCuadroM("Homenaje a Antrios"); 
+ 	 // "Antrios es un pintor ficticio de la obra de teatro Arte de Yasmina Reza" 
+	 lineaM(10, 1, true, 0); 
+	 lineaM(20, 1, true, 0); 
+	 lineaM(60, 1, true, 0); 
+	 nuevoCuadroM("Damero"); 
+ 	 rectanguloM(0, 0, 25, 25, 1); 
+	 rectanguloM(50, 0, 25, 25, 1); 
+	 rectanguloM(0, 50, 25, 25, 1); 
+	 rectanguloM(50, 50, 25, 25, 1); 
+	 rectanguloM(25, 25, 25, 25, 1); 
+	 rectanguloM(75, 25, 25, 25, 1); 
+	 rectanguloM(25, 75, 25, 25, 1); 
+	 rectanguloM(75, 75, 25, 25, 1); 
+	// Se liberan los recursos del entorno gráfico
 	terminarM();
 
 }
@@ -23,22 +61,36 @@ int main(int argc, char **argv){
  // Tabla de Simbolos
 /* 
 	 Nombre: grosor , Tipo: Entero, Valor: 4 
-	 Nombre: tiempoPausa , Tipo: Real, Valor: 1.890000 
+	 Nombre: mitad , Tipo: Entero, Valor: 50 
+	 Nombre: p , Tipo: Real, Valor: 1.770000 
+	 Nombre: x , Tipo: Entero, Valor: 25 
+	 Nombre: y , Tipo: Entero, Valor: 75 
 */
 
  // Tabla de Simbolos Recuadros 
 /* 
-	 Nombre: CuadroAzul , Tipo: Recuadro, Alto: 46 , Ancho: 46 , Color: 4 
-	 Nombre: FranjaNegra , Tipo: Recuadro, Alto: 32 , Ancho: 100 , Color: 1 
-	 Nombre: FranjaVerde , Tipo: Recuadro, Alto: 32 , Ancho: 100 , Color: 6 
-	 Nombre: RectanguloGris , Tipo: Recuadro, Alto: 27 , Ancho: 50 , Color: 2 
-	 Nombre: RectanguloRojo , Tipo: Recuadro, Alto: 46 , Ancho: 16 , Color: 3 
-	 Nombre: ZonaAmarilla , Tipo: Recuadro, Alto: 15 , Ancho: 30 , Color: 5 
+	 Nombre: Amarillo1 , Tipo: Recuadro, Alto: 19 , Ancho: 24 , Color: 5 
+	 Nombre: Amarillo2 , Tipo: Recuadro, Alto: 11 , Ancho: 24 , Color: 5 
+	 Nombre: Amarillo3 , Tipo: Recuadro, Alto: 25 , Ancho: 11 , Color: 5 
+	 Nombre: Azul1 , Tipo: Recuadro, Alto: 25 , Ancho: 24 , Color: 4 
+	 Nombre: Blanco1 , Tipo: Recuadro, Alto: 11 , Ancho: 4 , Color: 0 
+	 Nombre: Blanco2 , Tipo: Recuadro, Alto: 4 , Ancho: 11 , Color: 0 
+	 Nombre: Blanco3 , Tipo: Recuadro, Alto: 12 , Ancho: 4 , Color: 0 
+	 Nombre: Blanco4 , Tipo: Recuadro, Alto: 9 , Ancho: 4 , Color: 0 
+	 Nombre: Blanco5 , Tipo: Recuadro, Alto: 71 , Ancho: 10 , Color: 0 
+	 Nombre: CampoAzul , Tipo: Recuadro, Alto: 50 , Ancho: 50 , Color: 4 
+	 Nombre: CampoGris , Tipo: Recuadro, Alto: 100 , Ancho: 100 , Color: 2 
+	 Nombre: CampoVerde , Tipo: Recuadro, Alto: 50 , Ancho: 50 , Color: 6 
+	 Nombre: CuadradoNegro , Tipo: Recuadro, Alto: 25 , Ancho: 25 , Color: 1 
+	 Nombre: Negro1 , Tipo: Recuadro, Alto: 22 , Ancho: 23 , Color: 1 
+	 Nombre: Negro2 , Tipo: Recuadro, Alto: 12 , Ancho: 16 , Color: 1 
+	 Nombre: Rojo1 , Tipo: Recuadro, Alto: 43 , Ancho: 43 , Color: 3 
+	 Nombre: Rojo2 , Tipo: Recuadro, Alto: 25 , Ancho: 11 , Color: 3 
 */
 
  // Tabla de Simbolo de Lineas 
 /* 
-	 Nombre: LineaFina , Tipo: Linea, Grosor: 1  esHorizontal: True , Color: 2 
+	 Nombre: LineaFinaBlanca , Tipo: Linea, Grosor: 1  esHorizontal: True , Color: 0 
 	 Nombre: LineaH , Tipo: Linea, Grosor: 4  esHorizontal: True , Color: 1 
 	 Nombre: LineaV , Tipo: Linea, Grosor: 4  esHorizontal: False , Color: 1 
 */
